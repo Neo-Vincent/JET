e9d802395815c906222c4f04f3e1c79fdd81380f
Neo-Vincent <neo_vincent@outlook.com>
2016-08-04 23:31:49 +0800

    add disk usage

--- static/js/viewModels/chart.js
+++ static/js/viewModels/chart.js
@@ -1,125 +1,31 @@
-	requirejs.config({
-// Path mappings for the logical module names
-    paths: 
-    //injector:mainReleasePaths
-     {
-        'knockout': 'libs/knockout/knockout-3.4.0',
-        'knockout-amd-helpers': 'libs/knockout/knockout-amd-helpers.min',
-        'jquery': 'libs/jquery/jquery-2.1.3.min',
-        'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.11.4.min',
-        'promise': 'libs/es6-promise/promise-1.0.0.min',
-        'ojs': 'libs/oj/v2.0.1/min',
-        'ojL10n': 'libs/oj/v2.0.1/ojL10n',
-        'ojtranslations': 'libs/oj/v2.0.1/resources',
-        'signals': 'libs/js-signals/signals.min',
-        'text': 'libs/require/text',
-        'hammerjs': 'libs/hammer/hammer-2.0.4.min',
-        'moment': 'libs/moment/moment.min',
-        'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.0.min'
-    }
-    //endinjector
-    ,
-    // Shim configurations for modules that do not expose AMD
-    shim: {
-        'jquery': {
-            exports: ['jQuery', '$']
-        },
-        'maps': {
-            deps: ['jquery', 'i18n'],
-            exports: ['MVMapView']
+/**
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+define(['knockout', 'ojs/ojcore', 'data/data', 'ojs/ojknockout', 'ojs/ojmasonrylayout', 'ojs/ojchart', 'ojs/ojgauge'],
+    function (ko, oj, data)
+    {
+
+        function chartBase(id,dataType,chartType,chartTitle){
+            var self=this;
+            self.identifier=id;
+            self.dataType=dataType;
+            self.chartType=chartType;
+            self.chartTitle=chartTitle;
+            var converterFactory = oj.Validation.converterFactory("number");
+            var converter = converterFactory.createConverter({pattern: '#,##GB'});
+            this.converter = ko.observable(converter);
         }
-    },
-    // This section configures the i18n plugin. It is merging the Oracle JET built-in translation
-    // resources with a custom translation file.
-    // Any resource file added, must be placed under a directory named "nls". You can use a path mapping or you can define
-    // a path that is relative to the location of this main.js file.
-    config: {
-        ojL10n: {
-            merge: {
-                //'ojtranslations/nls/ojtranslations': 'resources/nls/menu'
-            }
+        function mychart(id,dataType,chartType,chartTitle){
+                chartBase.call(this,id,dataType,chartType,chartTitle);
         }
-    }
-});
+        mychart.prototype = new chartBase();
+        mychart.prototype.constructor = myChart;
 
 
-require(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojchart'],
-function(oj, ko, $)
-{   
-    function ChartModel() {
-        var self = this;
-
-        /* toggle button variables */
-        self.orientationValue = ko.observable('vertical');
-        
-        /* chart data */
-        self.items =  [3,  2,  3,  3,  2];
-        function getItems(val){
-            if(typeof val !== 'undefined'){
-                if (self.items.length > 10) {
-                    self.items.shift();
-                }
-                self.items.push(val);
-            }
-            return self.items;
+        function ChartModel() {
+            var self = this;
+            self.sysInfo=[];
         }
-        var lineSeries = [{name : "Series 1", items : getItems() }];
-    
-        var lineGroups = ["Group A"];
-
-
-        
-        this.lineSeriesValue = ko.observableArray(lineSeries);
-        this.lineGroupsValue = ko.observableArray(lineGroups);
-
-
-        self.updateButtonClick = function(data, event) {
-            var lineSeries = [{name : "Series 1", items : getItems() }];
-            self.lineSeriesValue(lineSeries);
-            return true;
-        };
-
-        function getValue() {
-            $.ajax(
-                {
-                    url: '/test/',
-                    type: 'POST',
-                    data: JSON.stringify({"require":true}),
-                    success: function (jsonResponse) {
-                        var objresponse = JSON.parse(jsonResponse);
-                        var lineSeries = [{name : "Series 1", items : getItems(objresponse["new"]) }];
-                        self.lineSeriesValue(lineSeries);
-                        console.log(objresponse);
-                    }
-                });
-        }
-
-        function getSysInfo() {
-            $.ajax(
-                {
-                    url: '/sysinfo',
-                    type: 'POST',
-                    data: JSON.stringify({"require":["CPU.info","CPU.status","Disk.info","Disk.status",
-                                            "Mem.mem","Mem.swap","Platform.info","Network.addrs","Network.if"]}),
-                    success: function (jsonResponse) {
-                        var objresponse = JSON.parse(jsonResponse);
-                        console.log(objresponse);
-                    }
-                });
-        }
-        window.setInterval(getSysInfo,1000);
-        /* toggle buttons*/
-        self.orientationOptions = [
-            {id: 'vertical', label: 'vertical', value: 'vertical', icon: 'oj-icon demo-line-vert'},
-            {id: 'horizontal', label: 'horizontal', value: 'horizontal', icon: 'oj-icon demo-line-horiz'}
-        ];
-    }
-    
-    var chartModel = new ChartModel();
-    
-    $(document).ready(
-	function(){
-            ko.applyBindings(chartModel, document.getElementById('chart-container'));
-	}
-    );
-});	
+        return ChartModel;
+    });
